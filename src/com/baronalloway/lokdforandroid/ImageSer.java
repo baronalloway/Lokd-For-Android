fdb14cc06db8a998ee92dd3a243388560ff4fdf5
Baron Alloway <baron@alloway.net>
2014-06-25 17:06:04 -0400

    Made ListView clickable and started Viewer Activity

--- src/com/baronalloway/lokdforandroid/ImageSer.java
+++ src/com/baronalloway/lokdforandroid/ImageSer.java
@@ -2,6 +2,7 @@ package com.baronalloway.lokdforandroid;
 
 public class ImageSer {
 	
+	//TODO: Write class so that it serializes the image that was taken, and another method to unserialize
 	
 
 }
--- src/com/baronalloway/lokdforandroid/ViewItem.java
+++ src/com/baronalloway/lokdforandroid/ViewItem.java
@@ -1,15 +1,53 @@
 package com.baronalloway.lokdforandroid;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class ViewItem extends Activity {
-
+	String password;
+	String selected;
+	EncryptionKey userKey;
+	List<WalletItem> myItems = new ArrayList<WalletItem>();
+	WalletItem selectedItem;
+	int a = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_item);
+		
+		
+		Intent i = getIntent();
+		
+		password = i.getStringExtra("password");
+		selected = i.getStringExtra("selected");
+		
+		try{
+		userKey = new EncryptionKey(password);
+		myItems = userKey.get(userKey.getKey(), userKey.getCipher(), userKey.getDCipher(), getApplicationContext());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		do{
+			selectedItem = myItems.get(a);
+			a++;
+		}while(!(selected.equals(myItems.get(a-1).getName())));
+		
+		setTitle(selectedItem.getName());
+		
+		
+		//TODO: code to display the selected item using the ImageSer and a view
+		
+		
 	}
 
 	@Override
